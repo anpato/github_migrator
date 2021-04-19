f2e2d72b53068cf2f2010169b87bd37727c24fde
Andre Pato <anpato1994@gmail.com>
2021-04-19 00:24:23 -0400

    reset code

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,9 +4,6 @@ from flask import request
 import os
 from flask_socketio import emit
 
-username = os.getenv('GH_USERNAME')
-password = os.getenv('GH_PASSWORD')
-
 
 def get_all(token, org, page, limit):
     gh = Github()
@@ -17,33 +14,30 @@ def get_all(token, org, page, limit):
 
 
 def clone(repos: list, target: str, token: str, dirname: str):
-    try:
-        storage_path = '{dirname}/tmp/{target}-repos'.format(
-            dirname=dirname, target=target)
-        cloned_repos = []
-        if not os.path.isdir(storage_path):
-            os.system("mkdir {}".format(storage_path))
-        count = 1
+    storage_path = '{dirname}/{target}-repos'.format(
+        dirname=dirname, target=target)
+    cloned_repos = []
+    if not os.path.isdir(storage_path):
+        os.mkdir(storage_path)
+    count = 1
+    for repo in repos:
+        try:
+            cmd: str = "git -C {storage} clone {clone_url} -q".format(
+                clone_url=repo['ssh_url'], storage=storage_path)
+            os.system(cmd)
+            path = "{storage}/{repo_name}".format(storage=storage_path,
+                                                  repo_name=repo['name'])
+            repo_data = {
+                "path": path, "desc": repo['description'], "name": repo['name']}
+            cloned_repos.append(repo_data)
+        except Exception as error:
+            print(error)
 
-        for repo in repos:
-            try:
-                cmd: str = "git -C {storage} clone {clone_url} -q".format(
-                    clone_url=repo['clone_url'].replace('https://', 'https://{username}:{password}@'.format(username=username, password=password)), storage=storage_path)
-                os.system(cmd)
-                path = "{storage}/{repo_name}".format(storage=storage_path,
-                                                      repo_name=repo['name'])
-                repo_data = {
-                    "path": path, "desc": repo['description'], "name": repo['name']}
-                cloned_repos.append(repo_data)
-            except Exception as error:
-                print(error)
-
-            emit('UploadProgress-{token}'.format(token=token), {"status": "Cloning",
-                                                                "progress": count}, namespace='/upload')
-            count += 1
-        return (cloned_repos, storage_path)
-    except Exception as error:
-        print('Mkdir Error', error)
+        emit('UploadProgress-{token}'.format(token=token), {"status": "Cloning",
+                                                            "progress": count}, namespace='/upload')
+        count += 1
+        print(count)
+    return (cloned_repos, storage_path)
 
 
 def create_repos(repos: list, token: str, out_org: str):
@@ -56,23 +50,21 @@ def create_repos(repos: list, token: str, out_org: str):
             "description": r["desc"]
         }
         try:
-
             res = gh.create_repo(body, token, out_org)
-            clone_url = res['clone_url'].replace(
-                'https://', 'https://{username}:{password}@'.format(username=username, password=password))
+            clone_url = res['ssh_url']
             print("\n")
             cmd: str = "cd {dir} && git push -u --mirror -q {url}".format(
                 dir=r['path'], url=clone_url)
             os.system(cmd)
-        except Exception as error:
-            print(error)
+        except:
+            pass
         emit('UploadProgress-{token}'.format(token=token), {"status": "Creating",
                                                             "progress": count}, namespace='/upload')
         count += 1
 
 
 def clear_dir(path):
-    os.system('rm -rf {path}'.format(path=path))
+    os.system('rm -rf ./{path}'.format(path=path))
 
 
 def get_token():
