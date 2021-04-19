faf3aa0d684d398e7752b2653f1fc9e62675e8b2
Andre Pato <anpato1994@gmail.com>
2021-04-18 23:57:03 -0400

    add os dirname

--- utils/__init__.py
+++ utils/__init__.py
@@ -14,8 +14,8 @@ def get_all(token, org, page, limit):
 
 
 def clone(repos: list, target: str, token: str, dirname: str):
-    storage_path = '{target}-repos'.format(
-        dirname=dirname, target=target).lower()
+    storage_path = '{dirname}/{target}-repos'.format(
+        dirname=dirname, target=target)
     cloned_repos = []
     if not os.path.isdir(storage_path):
         os.mkdir(storage_path)
