dce2b45650a57f66228235ed5f2b7f41dceb7c45
Andre Pato <anpato1994@gmail.com>
2021-04-19 12:54:25 -0400

    update commands

--- utils/__init__.py
+++ utils/__init__.py
@@ -18,7 +18,7 @@ def clone(repos: list, target: str, token: str, dirname: str):
         dirname=dirname, target=target)
     cloned_repos = []
     if not os.path.isdir(storage_path):
-        os.system('sudo mkdir {}'.format(storage_path))
+        os.system('mkdir {storage}'.format(storage=storage_path))
     count = 1
     for repo in repos:
         try:
