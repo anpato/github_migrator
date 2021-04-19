f968ec4eabbb00f95c30dc620d7f384982dd78f3
Andre Pato <anpato1994@gmail.com>
2021-04-18 22:57:05 -0400

    update dir

--- utils/__init__.py
+++ utils/__init__.py
@@ -14,7 +14,7 @@ def get_all(token, org, page, limit):
 
 
 def clone(repos: list, target: str, token: str, dirname: str):
-    storage_path = '{dirname}/{target}-repos'.format(
+    storage_path = './{target}-repos'.format(
         dirname=dirname, target=target)
     cloned_repos = []
     if not os.path.isdir(storage_path):
